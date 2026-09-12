084f95fc34b184817e608cfd34e481b00ca2e36b
lweekyuu-web <lweekyuu@gmail.com>
2026-09-12 20:47:20 +0200

    Delete pourriture-org-fixed/pourriture.org-main directory

--- pourriture-org-fixed/pourriture.org-main/pourture-org-v3/pages/catalogue.js
+++
@@ -1,7 +0,0 @@
-export default function CatalogueRedirect() {
-  return null;
-}
-
-export async function getServerSideProps() {
-  return { redirect: { destination: '/catalog', permanent: true } };
-}
